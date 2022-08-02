ff391c45f977972e1deec48f2620f821fda447c7
Naduladisanayaka494 <it20247140@my.sliit.lk>
2022-08-02 13:25:14 +0530

    second day

---
+++ src/app/components/products/products.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-products',
+  template: '<div><h3>This is template</h3></div>',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
